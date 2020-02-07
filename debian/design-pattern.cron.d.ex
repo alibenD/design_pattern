1e40d3890145e7fa9385b72da9c1d50203db3346
Aliben <aliben.develop@gmail.com>
2020-02-07 23:42:08 +0800

    Updated debian

--- debian/design-pattern.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the design-pattern package
-#
-0 4	* * *	root	[ -x /usr/bin/design-pattern_maintenance ] && /usr/bin/design-pattern_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(design-pattern):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="design-pattern" command="/usr/bin/design-pattern"
